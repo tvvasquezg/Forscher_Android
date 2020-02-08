686255405858fe0eff9070d9cc94491c52666d64
gmojica@unal.edu.co <55957159+imgamp@users.noreply.github.com>
2020-02-08 01:37:14 -0500

    Creacion Hashing

--- app/src/main/java/com/example/proyectofinal/estructura_de_datos/HashSearch.java
+++ app/src/main/java/com/example/proyectofinal/estructura_de_datos/HashSearch.java
@@ -17,15 +17,15 @@ public class HashSearch<T> implements Serializable {
     }
 
     public int FunctionHash(String key, HashSearch h){
-        int hashVal =0;
+        int hashVa =0;
         for (int i= 0; i < key.length();i++){
-            hashVal = 45* hashVal+ key.charAt(i);
+            hashVa = 45* hashVa+ key.charAt(i);
         }
-        hashVal %= h.size;
-        if(hashVal<0){
-            hashVal += h.size;
+        hashVa %= h.size;
+        if(hashVa<0){
+            hashVa += h.size;
         }
-        return hashVal;
+        return hashVa;
     }
 
 }
